0eaba454d24e361f9f9a891b9fb85159c957f75d
Hank <ezp9xp@virginia.edu>
2023-09-25 22:46:15 -0400

    New file GuessResultTest.java created. Currently three tests, one test getLetterResult(), which is not yet complete. getGuess() and getAnswer() are finish implement.

---
+++ src/test/java/edu/virginia/sde/hw2/wordle/GuessResultTest.java
@@ -0,0 +1,23 @@
+package edu.virginia.sde.hw2.wordle;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class GuessResultTest {
+    @Test
+    public void test_all_correct_getLetterResult(){
+        var test = new GuessResult("apple","apple");
+    }
+    @Test void test_getGuess(){
+        var test = new GuessResult("apple","apple");
+        assertEquals("apple",test.getGuess());
+        assertNotEquals("ascde",test.getGuess());
+    }
+    @Test void test_getAnswer(){
+        var test = new GuessResult("apple","apple");
+        assertEquals("apple",test.getAnswer());
+        assertNotEquals("ascde",test.getAnswer());
+    }
+}
